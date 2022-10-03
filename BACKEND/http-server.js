e1f4b9c752f7d79e6f9f3ecb4292b5cd9fe4ea72
Nome Sobrenome <seu_email@email.com>
2022-10-03 15:43:53 -0300

    setting up CORS policy

--- BACKEND/http-server.js
+++ BACKEND/http-server.js
@@ -19,6 +19,17 @@ class HttpServer {
         this._server.listen(this._httpServerPort, '0.0.0.0');
         this._server.on('error', this._onServerError);
         this._server.on('listening', this._onServerListening);
+        this._app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header(
+                'Access-Control-Allow-Headers',
+                'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+            );
+            res.header(
+                'Access-Control-Allow-Methods',
+                'GET, POST, PUT, PATCH, DELETE'
+            );
+        });
     }
 
     _onServerListening() {
@@ -44,17 +55,6 @@ class HttpServer {
         this._app.set('port', this._httpServerPort);
         this._app.use(bodyParser.json({limit: '2mb'}));
         this._app.use(bodyParser.urlencoded({limit: '2mb', extended: true}));
-        this._app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header(
-                'Access-Control-Allow-Headers',
-                'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
-            );
-            res.header(
-                'Access-Control-Allow-Methods',
-                'GET, POST, PUT, PATCH, DELETE'
-            );
-        });
     }
 
     _setupControllers() {
