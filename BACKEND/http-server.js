f1d04d3f5a96d56c306e75c68acafd1cb64bcadf
Nome Sobrenome <seu_email@email.com>
2022-10-03 17:01:50 -0300

    config

--- BACKEND/http-server.js
+++ BACKEND/http-server.js
@@ -20,7 +20,24 @@ class HttpServer {
         this._server.listen(this._httpServerPort, '0.0.0.0');
         this._server.on('error', this._onServerError);
         this._server.on('listening', this._onServerListening);
-        this._app.use(CORS);
+        this._app.use(function (req, res, next) {
+
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', '*');
+
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', true);
+
+            // Pass to next layer of middleware
+            next();
+        });
     }
 
     _onServerListening() {
