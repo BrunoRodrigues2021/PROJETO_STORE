a5f91cacc32bb6c29015498bebfea8ec6df13c1d
Nome Sobrenome <seu_email@email.com>
2022-10-05 23:14:29 -0300

    aws tests

--- BACKEND/scripts/before_install.sh
+++ BACKEND/scripts/before_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo yum install nodejs
-sudo yum install npm
-sudo npm install pm2 -g
-sudo cd ./home/ec2-user
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+nvm install 16
 sudo rm -r -f store-backend
 sudo mkdir store-backend
